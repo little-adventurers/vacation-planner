73dff77eaa07231df03f03c714bb87ac11072394
Jason Vou <vou2006@gmail.com>
2019-10-20 22:17:39 -0700

    get user input for information

--- console/main.py
+++ console/main.py
@@ -1,5 +1,16 @@
 def main():
-    print("Hello, world!");
+    print("Hello, world!")
+    user_name = input("What is your full name? ")
+    user_addr_st = input("What is your street address? ")
+    #user_addr_zipcode = input("What is the zip code? ")
+    user_dob = input("What is your birthdate (mm/dd/yyyy)? ")
+
+    setUser(user_name, user_addr_st, user_dob)
+
+def setUser(name, address, dob):
+    print("Hello, " + name)
+    print("You live at: " + address)
+    print("Your birthdate is: " + dob)
 
 # Treats the main function as the base function
 if __name__ == "__main__":
