274867fa497eafa46737240c47f89bf4023ab728
Jason Vou <vou2006@gmail.com>
2020-04-04 20:53:56 -0700

    broken: add itineraries to home view

--- vacationplanner/views.py
+++ vacationplanner/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Vacation, Itinerary
+from .models import Vacation, Itinerary, Post
 
 def home(request):
     vacations = Vacation.objects.all()
     return render(request, 'home.html', {'vacations': vacations})
 
-def vacation_itinerary(request, pk):
+def vacation_itineraries(request, pk):
+    foo = 1/0
     vacation = Vacation.objects.get(pk=pk)
-    return render(request,'vacations.html',{'vacation':vacation})
+    return render(request,'itinerary.html',{'vacation':vacation})
